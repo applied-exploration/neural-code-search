f9b7a0eb67166396e06a6732b94004e5b3302079
Daniel Szemerey <account@szemerey.eu>
2022-10-19 20:43:56 +0200

    feat: Added an api endpoint and flask.

---
+++ cosine_similarity.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from constants import Const, DFCols
+from torch.nn import CosineSimilarity
+import torch
+from typing import Tuple, List
+import numpy as np
+
+
+def load_library(path: str) -> pd.DataFrame:
+    df = pd.read_parquet(path)
+
+    return df
+
+
+class CosineSimilaritySearch:
+    def __init__(self, library: pd.DataFrame, k: int) -> None:
+        self.library = library[~library[DFCols.embedded_feature.value].isnull()].reset_index()
+        self.vectors = torch.stack(
+            [
+                torch.from_numpy(vector).float()
+                for vector in self.library[DFCols.embedded_feature.value].to_list()
+            ],
+            dim=1,
+        )
+        self.cos = CosineSimilarity(dim=1)
+        self.k = k
+
+    def get_similarity(
+        self, embedded_texts: torch.Tensor
+    ) -> List[int]:
+
+        output = self.cos(embedded_texts.unsqueeze(0), self.vectors.T)
+
+        k_best_similarity_value, k_best_indices = torch.topk(output, dim=0, k=self.k)
+
+        return k_best_indices.tolist()
+
+    def snippet_lookup(self, indicies:List[int])->pd.DataFrame:
+        return self.library.iloc[indicies]
+        
+        
+        
--- run.py
+++ run.py
@@ -1,3 +1,24 @@
 if __name__ == '__main__':
     raise Exception('run.py: Not Implemented')
 
+<<<<<<< Updated upstream
+=======
+
+def search(query: str) -> None:
+    search_module = NeuralSearch()
+    search_module.predict(query)
+
+
+
+if __name__ == "__main__":
+    # search('def hello_s(s: str):\n    print(f"Hello s")')
+    search(
+        """
+        L = x.tolist()
+        if len(L) > 1:
+            return L
+        else:
+            return L[0]
+        """
+    )
+>>>>>>> Stashed changes
