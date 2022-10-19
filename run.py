263abf3da3de0d61755aecbb6e1aeabc36a6ed94
Daniel Szemerey <account@szemerey.eu>
2022-10-19 22:27:51 +0200

    feat: Added an api endpoint and flask. (#12)

--- run.py
+++ run.py
@@ -6,5 +6,16 @@ def search(query: str) -> None:
     search_module.predict(query)
 
 
+
 if __name__ == "__main__":
-    search('def hello_s(s: str):\n    print(f"Hello s")')
+    # search('def hello_s(s: str):\n    print(f"Hello s")')
+    search(
+        """
+        L = x.tolist()
+        if len(L) > 1:
+            return L
+        else:
+            return L[0]
+        """
+    )
+
